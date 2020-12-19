d36926eb957e1999eea158f409e0573ae71f72f6
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-12-19 21:52:42 +0100

    popravio

---
+++ src/rs/math/oop1/z170200/lambdaIzrazi/z01/osobePretragaPrikaz/KriterijumPretrageOsoba.java
@@ -0,0 +1,5 @@
+package rs.math.oop1.z170200.lambdaIzrazi.z01.osobePretragaPrikaz;
+
+public interface KriterijumPretrageOsoba {
+    boolean proveri(Osoba osoba);
+}
--- src/rs/math/oop1/z170201/funkcionalniInterfejsi/z03/osobe/CheckPerson.java
+++
@@ -1,7 +0,0 @@
-package rs.math.oop1.z170201.funkcionalniInterfejsi.z03.osobe;
-
-@FunctionalInterface
-interface CheckPerson {
-	boolean test(Person p);
-}
-
