bc8bc1f56b5992cc71376a40519a8cc20f74b784
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-12-19 13:51:40 +0100

    promenio nazive klasa i paketa

--- src/rs/math/oop1/z190201/funkcionalniInterfejsi/z02/osobe/CheckPerson.java
+++
@@ -1,6 +0,0 @@
-package rs.math.oop1.z190201.funkcionalniInterfejsi.z02.osobe;
-
-interface CheckPerson { 
-	boolean test(Person p); 
-}
-
--- src/rs/math/oop1/z190201/funkcionalniInterfejsi/z03/osobe/CheckPerson.java
+++
@@ -1,7 +0,0 @@
-package rs.math.oop1.z190201.funkcionalniInterfejsi.z03.osobe;
-
-@FunctionalInterface
-interface CheckPerson {
-	boolean test(Person p);
-}
-
